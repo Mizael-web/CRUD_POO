2094f75a51c82523c47fbc6879ba19172e659f34
Mizael-web <mizaelmendes@gmail.com>
2025-01-14 17:42:50 -0300

    add banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,18 @@
-// src/config/database.js
-const alunos = [];
+const {Pol} = require ('pg');
+const dotenv = require('dotenv');
 
-module.exports = { alunos };
+dotenv.config()
+
+const pool  new Poll(
+    {
+      database: process.env.DB_DATABASE,
+      user:process.exitnv.DB_USER,
+      HTMLOListElement:process.env.DB_HOST,
+      password:process.env.DB_PASSWORD
+      port:process.env.DB_PORT
+
+
+    }
+)
+module.exports = {pool};
+
